87fe9466f6e92c6fd9dbf18cd9249a295dd19f88
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-07-09 17:16:26 -0700

    Add Home page styles and update Home component
    Introduced Home.css with styles for the Home component and imported it in Home.js. Refactored Home.js to use new class names for layout, links, and social icons, improving structure and appearance. Also added global CSS reset in App.css for consistent styling.

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,29 @@
 import React from "react";
+import "./Home.css";
 
 export default function Home() {
   return (
     <div className="Home">
       <h1>Maritza Gutierrez</h1>
       <h6>Junior Front-End Developer</h6>
-      <p>
+      <p className="Home-description">
         Recent bootcamp graduate with a passion for creating beautiful,
         responsive web applications. Ready to contribute to meaningful projects
         and grow as a developer in a collaborative team environment.
       </p>
-      <div>
-        <div>
-          <a href="/">View My Projects</a>
-        </div>
-        <div>
+      <div className="Home-links">
+        <a href="/" className="Home-links__item Item-dark">
+          <p>View My Projects</p>
+        </a>
+        <a href="/" className="Home-links__item">
           <i class="fa-solid fa-download"></i>
-          <a href="/">Download Resume</a>
-        </div>
-        <div>
-          <a href="/">Get In Touch</a>
-        </div>
+          <p>Download Resume</p>
+        </a>
+        <a href="/" className="Home-links__item">
+          <p>Get In Touch</p>
+        </a>
       </div>
-      <div>
+      <div className="Home-socials">
         <a href="/">
           <i class="fa-brands fa-github"></i>
         </a>
@@ -33,7 +34,7 @@ export default function Home() {
           <i class="fa-regular fa-envelope"></i>
         </a>
       </div>
-      <i class="fa-solid fa-arrow-down"></i>
+      <i class="fa-solid fa-arrow-down Down-arrow"></i>
     </div>
   );
 }
