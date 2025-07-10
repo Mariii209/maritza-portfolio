564a08bcba0786babd3d9fff02f908961a992dc5
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-07-09 23:07:35 -0700

    Enhance WhatDrivesMe section styles and content
    Updated WhatDrivesMe.js to use unique icons, headings, and descriptions for each drive item. Improved styling in WhatDrivesMe.css for layout, colors, and typography, and made minor cleanup in About.css. These changes provide a clearer, more visually appealing presentation of the section.

--- src/WhatDrivesMe.js
+++ src/WhatDrivesMe.js
@@ -6,8 +6,8 @@ export default function WhatDrivesMe() {
     <div className="WhatDrivesMe">
       <div className="Drive-item">
         <div className="DriveItem-navigation">
-          <i class="fas fa-chevron-left"></i>
-          <i class="fas fa-chevron-right"></i>
+          <i class="fas fa-chevron-left Learning"></i>
+          <i class="fas fa-chevron-right Learning"></i>
         </div>
         <h6>Learning & Growth</h6>
         <p>
@@ -17,24 +17,21 @@ export default function WhatDrivesMe() {
       </div>
       <div className="Drive-item">
         <div className="DriveItem-navigation">
-          <i class="fas fa-chevron-left"></i>
-          <i class="fas fa-chevron-right"></i>
+          <i class="fa-solid fa-palette Creativity"></i>
         </div>
-        <h6>Learning & Growth</h6>
+        <h6>Fresh Perspective</h6>
         <p>
-          Constantly expanding my knowledge through hands-on projects and online
-          resources.
+          Bringing creativity and user-focused thinking to every project I work
+          on.
         </p>
       </div>
       <div className="Drive-item">
         <div className="DriveItem-navigation">
-          <i class="fas fa-chevron-left"></i>
-          <i class="fas fa-chevron-right"></i>
+          <i class="fa-solid fa-bolt Eager"></i>
         </div>
-        <h6>Learning & Growth</h6>
+        <h6>Enthusiasm</h6>
         <p>
-          Constantly expanding my knowledge through hands-on projects and online
-          resources.
+          Eager to contribute to meaningful projects and grow as a developer.
         </p>
       </div>
     </div>
