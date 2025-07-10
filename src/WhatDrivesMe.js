cd2f6a7ab10f2cff0b854f760e1ba7a431a3e1eb
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-07-09 22:20:33 -0700

    Add Drive items layout to WhatDrivesMe component
    Updated WhatDrivesMe.js to render three Drive-item sections, each with navigation icons, headings, and descriptions. Adjusted WhatDrivesMe.css to use flex layout and padding for improved styling.

--- src/WhatDrivesMe.js
+++ src/WhatDrivesMe.js
@@ -2,5 +2,41 @@ import React from "react";
 import "./WhatDrivesMe.css";
 
 export default function WhatDrivesMe() {
-  return <div className="WhatDrivesMe"></div>;
+  return (
+    <div className="WhatDrivesMe">
+      <div className="Drive-item">
+        <div className="DriveItem-navigation">
+          <i class="fas fa-chevron-left"></i>
+          <i class="fas fa-chevron-right"></i>
+        </div>
+        <h6>Learning & Growth</h6>
+        <p>
+          Constantly expanding my knowledge through hands-on projects and online
+          resources.
+        </p>
+      </div>
+      <div className="Drive-item">
+        <div className="DriveItem-navigation">
+          <i class="fas fa-chevron-left"></i>
+          <i class="fas fa-chevron-right"></i>
+        </div>
+        <h6>Learning & Growth</h6>
+        <p>
+          Constantly expanding my knowledge through hands-on projects and online
+          resources.
+        </p>
+      </div>
+      <div className="Drive-item">
+        <div className="DriveItem-navigation">
+          <i class="fas fa-chevron-left"></i>
+          <i class="fas fa-chevron-right"></i>
+        </div>
+        <h6>Learning & Growth</h6>
+        <p>
+          Constantly expanding my knowledge through hands-on projects and online
+          resources.
+        </p>
+      </div>
+    </div>
+  );
 }
