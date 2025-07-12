ef3fd9a18e523255ee5a11949b787ef821f82eca
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-07-12 11:36:21 -0700

    Add contact form to Contact page
    Introduced a contact form with fields for name, email, subject, and message, along with a send button. This allows users to send messages directly from the Contact page.

--- src/Contact.js
+++ src/Contact.js
@@ -11,6 +11,37 @@ export default function Contact() {
           and anyone interested in collaborating on projects.
         </p>
       </div>
+      <div>
+        <div className="Contact-form">
+          <h4>Send me a message</h4>
+          <div>
+            <div>
+              <label htmlFor="">Name</label>
+              <input type="text" placeholder="Your Name" />
+            </div>
+            <div>
+              <label htmlFor="">Email</label>
+              <input type="text" placeholder="your.email@example.com" />
+            </div>
+          </div>
+          <div>
+            <label htmlFor="">Subject</label>
+            <input type="text" name="" id="" placeholder="What's this about?" />
+          </div>
+          <div>
+            <label htmlFor="">Message</label>
+            <textarea
+              name=""
+              id=""
+              placeholder="Tell me about your project or opportunity..."
+            ></textarea>
+          </div>
+          <button>
+            <i class="fa-regular fa-paper-plane"></i>
+            <p>Send Message</p>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
