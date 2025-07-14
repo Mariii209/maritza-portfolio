b2e45742864fe37ed3e610d2799ca861b8383c08
Mariii209 <maritza.gutierrez30@yahoo.com>
2025-07-14 13:47:38 -0700

    Move Contact Information into separate Information component with dedicated styles
     -Extracted the contact info into Information.js
    -Styled it in Information.css
    -Cleaned up Contact.js and Contact.css

--- src/Contact.js
+++ src/Contact.js
@@ -54,38 +54,7 @@ export default function Contact() {
               <p>Send Message</p>
             </button>
           </div>
-          <div className="Contact-information">
-            <div>
-              <h6>Contact Information</h6>
-              <div>
-                <i className="fa-solid fa-envelope"></i>
-                <p>maritza.gutierrez115@gmail.com</p>
-              </div>
-              <div>
-                <i className="fa-solid fa-location-dot"></i>
-                <p>Modesto, CA</p>
-              </div>
-            </div>
-            <div>
-              <h6>Available for Opportunities</h6>
-              <p>
-                I'm currently seeking junior front-end developer positions and
-                would love to hear about yours.
-              </p>
-              <a href="/">Schedule a Call</a>
-              <a href="/">
-                <i class="fa-solid fa-download"></i>
-                <p>Download Resume</p>
-              </a>
-            </div>
-            <div>
-              <h6>Response Time</h6>
-              <p>
-                I typically respond to messages within 24 hours. Looking forward
-                to hearing from you!
-              </p>
-            </div>
-          </div>
+          <Information />
         </div>
       </div>
     </div>
--- src/Information.js
+++ src/Information.js
@@ -2,5 +2,43 @@ import React from "react";
 import "./Information.css";
 
 export default function Information() {
-  return <div></div>;
+  return (
+    <div className="Information">
+      <div className="Contact-info">
+        <h6>Contact Information</h6>
+        <div className="Email">
+          <i className="fa-solid fa-envelope"></i>
+          <p>maritza.gutierrez115@gmail.com</p>
+        </div>
+        <div className="Location">
+          <i className="fa-solid fa-location-dot"></i>
+          <p>Modesto, CA</p>
+        </div>
+      </div>
+      <div className="Available">
+        <h6>Available for Opportunities</h6>
+        <p>
+          I'm currently seeking junior front-end developer positions and would
+          love to hear about yours.
+        </p>
+        <div className="Available-links">
+          <a href="/" className="Schedule">
+            Schedule a Call
+          </a>
+          <a href="/" className="Download-resume">
+            <i className="fa-solid fa-download"></i>
+            Download Resume
+          </a>
+        </div>
+      </div>
+
+      <div className="Response-time">
+        <h6>Response Time</h6>
+        <p>
+          I typically respond to messages within 24 hours. Looking forward to
+          hearing from you!
+        </p>
+      </div>
+    </div>
+  );
 }
